ebd556ca0eb4b0ecbc9791b866e27f495d5ca38e
partyhardeee <danseriv56@gmail.com>
2022-07-22 18:27:03 +0500

    Поменял расположение

--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -48,7 +48,7 @@ public class YandexTest {
         /*
         Не получается сделать путь к драйверу начиная с папки проекта
          */
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\IdeaProjects\\Selenium_Maven_TestNG_Allure\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         return driver = new ChromeDriver();
     }
     @AfterClass (alwaysRun= true)
